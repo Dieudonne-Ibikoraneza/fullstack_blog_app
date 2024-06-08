237847a70a678e7870e028be5a3c7c063e82ee46
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-06-08 21:17:56 +0200

    Initial Commit

---
+++ client/src/components/index.js
@@ -0,0 +1,12 @@
+import Loading from "./Loading";
+import Logo from "./Logo";
+import Button from "./Button";
+import Divider from "./Divider";
+import Inputbox from "./InputBox";
+import Banner from "./Banner"
+import Card from "./Card";
+import Pagination from "./Pagination";
+import PopularPosts from "./PopularPosts";
+import PopularWriters from "./PopularWriters";
+
+export { Loading, Logo, Button, Divider, Inputbox, Banner, Card, PopularPosts, PopularWriters , Pagination};
