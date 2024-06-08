863a4bdc265541a5fb9e5f36e4b4e9140e312210
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-06-08 21:23:52 +0200

    Finished the utils folder setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Outlet } from "react-router-dom";
+import {
+  BlogDetails,
+  CategoriesPage,
+  Home,
+  LoginPage,
+  SignupPage,
+  WriterPage,
+} from "./pages";
+import { Loading } from "./components";
 
+function Layout() {
+  return (
+    <div className="w-full flex flex-col min-h-screen px-4 md:px-10 2xl:px-28">
+      {/* <Navbar /> */}
+      <div className="flex-1">
+        <Outlet />
+      </div>
+      {/* <Footer /> */}
+    </div>
+  );
+}
 function App() {
-  const [count, setCount] = useState(0)
+  const theme = "dark";
+  const isLoading = false;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className={theme}>
+      <div className="w-full min-h-screen relative bg-white dark:bg-[#020b19]">
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/category" element={<CategoriesPage />} />
+            <Route path="/:slug/:id?" element={<BlogDetails />} />
+            <Route path="/writer/:id" element={<WriterPage />} />
+          </Route>
+
+          <Route path="/sign-in" element={<LoginPage />} />
+          <Route path="/sign-up" element={<SignupPage />} />
+        </Routes>
+
+        {isLoading && <Loading />}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <GoogleOAuthProvider clientId={clientId}>
+        <App />
+        <ToastContainer
+          position="top-right"
+          closeOnClick={true}
+          reverseOrder={false}
+          toastClassName="custom-toast"
+        />
+      </GoogleOAuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
